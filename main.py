cf36cedd2f9e830fe743de5dba2018509f0be3e7
Ram Saravanan <156893636+Ram-Saravanan@users.noreply.github.com>
2024-10-06 13:53:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,8 +9,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 api_key = os.getenv('GOOGLE_API_KEY')
-tess_path = os.getenv("TESSERACT_PATH")
-pytesseract.pytesseract.tesseract_cmd = tess_path
+pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
 
 
 st.set_page_config(page_title="Quiz Generation App", layout="wide")
